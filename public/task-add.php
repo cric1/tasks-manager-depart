6dccfe174d138061fc5dc84e7e771c6658479ba2
Jacob <cricclmao@gmail.com>
2024-10-06 17:06:56 -0400

    change require

--- public/task-add.php
+++ public/task-add.php
@@ -5,53 +5,7 @@
 
 <body>
     <?php require "../views/header.php" ?>
-
-    <div class="container mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h2>Ajouter une tâche</h2>
-                        <form method="GET">
-                            <div class="mb-3">
-                                <label for="title" class="form-label">Titre</label>
-                                <input type="text" class="form-control" id="title" name="title" value="">
-                                <span class="help-inline"></span>
-                            </div>
-                            <div class="mb-3">
-                                <label for="category" class="form-label">Catégorie</label>
-                                <select id='category' name='category' class='form-select'>
-                                    <option value="" disabled selected>-- Veuillez faire une sélection --</option>
-                                    <option value="">awddaw</option>
-                                </select>
-                                <span class="help-inline"></span>
-                            </div>
-                            <div class="mb-3">
-                                <label for="date" class="form-label">Date</label>
-                                <input type="date" class="form-control" id="date" name="date" value="">
-                                <span class="help-inline"></span>
-                            </div>
-                            <div class="mb-3">
-                                <label for="status" class="form-label">Statut</label>
-                                <select id="status" name="status" class="form-select">
-                                    <option value="" disabled selected>-- Veuillez faire une sélection --</option>
-                                    <option value="">awddaw</option>
-                                </select>
-                                <span class="help-inline"></span>
-                            </div>
-                            <div class="mb-3">
-                                <label for="description" class="form-label">Description</label>
-                                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
-                                <span class="help-inline"></span>
-                            </div>
-                            <button type="submit" class="btn btn-success">Ajouter</button>
-                            <a class="btn btn-primary" href=""><span class="bi-arrow-left"></span> Annuler</a>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php require "../views/body.php" ?>
     <?php
     require '../src/functions.php';
     session_start();
