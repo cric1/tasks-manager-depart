89c75c0af585c2b298c5ec20347ee5ddde51d977
Jacob <cricclmao@gmail.com>
2024-10-06 20:10:55 -0400

    simplifie task index un peu

--- public/task-index.php
+++ public/task-index.php
@@ -15,43 +15,33 @@
                         <div class="col-md-3">
                             <!-- Catégorie -->
                             <select class="form-select"> 
-
-                                 <option value="" selected hidden>Toutes les catégories</option>
-                               
-                                 <?php $categories = readFromFile("data/categories.json");?>
-                                 
-                                 <?php foreach($categories as $category) : ?>
+                                <option value="" selected hidden>Toutes les catégories</option>
+                                <?php $categories = readFromFile("data/categories.json");?>
+                                <?php foreach($categories as $category) : ?>
                                     <option value=<?= $category['name'] ?>> <?= $category['name'] ?></option>
-                                 <?php endforeach ?> 
-
+                                <?php endforeach ?> 
                             </select>
                         </div>
 
                         <div class="col-md-3">
                             <!-- Statut -->
                             <select class="form-select">
-
-                                 <option value="" selected hidden>Tous les status</opt>
-                                 <?php $statusFile = readFromFile("data/status.json");?>
-                                 
-                                 <?php foreach($statusFile as $status) : ?>
+                                <option value="" selected hidden>Tous les status</option>
+                                <?php $statusFile = readFromFile("data/status.json");?>
+                                <?php foreach($statusFile as $status) : ?>
                                     <option value=<?= $status['name'] ?>> <?= $status['name'] ?></option>
-                                 <?php endforeach ?>
-                                 
+                                <?php endforeach ?>
                             </select>
                         </div>
 
                         <div class="col-md-4">
                             <!-- Recherche par texte -->
                             <input class="form-control" type="text" id="searchText" name="searchText" placeholder="Recherche par texte">
-
                         </div>
 
                         <div class="col-md-2">
-                            
                             <!-- ****** Manque la fonction (filtrer selon les valeurs)-->
                             <button class="btn btn-primary" type="button">Filtrer</button>
-
                         </div>
                     </div>
                 </form>
@@ -62,7 +52,6 @@
                 </form>
                 <!-- ****** -->
 
-                </form>
                 <?php 
                 session_start();
                 if(isset($_SESSION['username'])) {
@@ -73,11 +62,10 @@
                     exit();
                 }
                 ?>
-               
-                
-                <?php foreach($tasks as $task) : ?>
-                <!-- TODO : Liste des tâches -->
+
                 <div class="row g-3">
+                    <?php foreach($tasks as $task) : ?>
+                    <!-- TODO : Liste des tâches -->
                     <div class="col-12 col-md-6 col-lg-4 mb-4">
                         <div class="card card-custom">
                             <div class="card-body">
@@ -85,31 +73,21 @@
                                 <h6 class="card-subtitle mb-2 text-muted"> <?= $task["category"]?> - <?= $task["date"] . $task["status"]?> </h6>
                                 <p class="card-text"> <?= $task["description"]?> </p>
                                 <div class="container">
-
                                     <!-- TODO : Modifier une tâche -->
-                                    <form action="" method="POST" class="d-inline-block">
+                                    <form action="task-edit.php" method="POST" class="d-inline-block">
                                         <button class="btn btn-warning" type="submit">Modifier</button>
-                                         <?php  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                                                     redirect("task-edit.php");
-                                                }
-                                         ?> 
                                     </form>
 
                                     <!-- TODO : Supprimer une tâche -->
-                                    <form action="" method="POST" class="d-inline-block">
+                                    <form action="task-delete.php" method="POST" class="d-inline-block">
                                         <button class="btn btn-danger" type="submit">Supprimer</button>
-                                        <?php  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                                                    redirect("task-delete.php");
-                                                }
-                                            ?> 
                                     </form>
-
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <?php endforeach ?>
                 </div>
-                <?php endforeach ?>
             </div>
         </div>
         
