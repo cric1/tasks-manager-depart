22265830be025dd1f4bdadfb47ff61f13220ff35
Jacob <cricclmao@gmail.com>
2024-10-06 22:04:19 -0400

    g tt pt

--- src/functions.php
+++ src/functions.php
@@ -17,22 +17,12 @@ function readFromFile(string $filename): array {
 }
 
 function writeToFile(string $filename, array $data): void {
-    if (file_exists($filename)) {
-        file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
-    } else {
-        echo "Le fichier n'existe pas: " . $filename; 
-    }
+file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
 }
 
 function addTask(string $user, array $newTask): void {
     $filename = 'data/' . $user . '-tasks.json';
     $tasks = readFromFile($filename);
-    
-    if (empty($tasks)) {
-        echo "Impossible d'ajouter une tâche. Le fichier " . $filename . " est introuvable.";
-        return;
-    }
-    
     $tasks[] = $newTask;
     writeToFile($filename, $tasks);
 }
@@ -41,9 +31,9 @@ function updateTask(string $filename, int $index, array $updatedTask): void {
     
 }
 
-function deleteTask($user, $taskId) {
+function deleteTask($user, $taskNum) {
     $tasks = readFromFile('data/' . $user . '-tasks.json'); 
-    unset($tasks[$taskId]); 
+    unset($tasks[$taskNum]); 
     return writeToFile("data/{$user}-tasks.json", $tasks);
 }
 
