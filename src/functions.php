904e6fc0ed88e77200a65586dd80b16f3346785c
Jacob <cricclmao@gmail.com>
2024-10-06 22:26:42 -0400

    opti

--- src/functions.php
+++ src/functions.php
@@ -1,36 +1,25 @@
 <?php
-
-
-
 function redirect($url) {
     header("location: " . $url);
     exit();
 }
 
 function readFromFile(string $filename): array {
-    if (file_exists($filename)) {
         $file = file_get_contents($filename);
-        return json_decode($file, true) ?? [];
-    }
-    echo "Le fichier n'existe pas: " . $filename; 
-    return [];
+        return json_decode($file, true);
 }
-
 function writeToFile(string $filename, array $data): void {
 file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
 }
-
 function addTask(string $user, array $newTask): void {
     $filename = 'data/' . $user . '-tasks.json';
     $tasks = readFromFile($filename);
     $tasks[] = $newTask;
     writeToFile($filename, $tasks);
 }
-
 function updateTask(string $filename, int $index, array $updatedTask): void {
     
 }
-
 function deleteTask($user, $taskNum):void {
     $tasks = readFromFile('data/' . $user . '-tasks.json'); 
     unset($tasks[$taskNum]); 
@@ -38,6 +27,8 @@ function deleteTask($user, $taskNum):void {
     redirect('task-index.php');
 }
 
-function renderOptions(string $file, string $itemSelected = "") : string {
+
+//jsp ca sert a quoi ca
+// function renderOptions(string $file, string $itemSelected = "") : string {
    
-}
+// }
