ad9d7a195b8d8647dad5dac765e20b60b95c2e99
Kevin Larivée <kevinlarivee360@gmail.com>
2024-10-08 12:21:19 -0400

    fonction de merde

--- src/functions.php
+++ src/functions.php
@@ -28,3 +28,6 @@ function deleteTask($user, $taskNum):void {
     unset($tasks[$taskNum]); 
     writeToFile("data/{$user}-tasks.json", $tasks);
 }
+function renderOptions(string $file, string $itemSelected = ""):string {
+   
+}
